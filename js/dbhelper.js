6b564780d7bc9a70355854d0456807fa3040e501
mzlovelyj <liljp064@gmail.com>
2018-10-03 23:55:51 -0400

    almost done

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,8 +8,9 @@ class DBHelper {
      * Change this to restaurants.json file location on your server.
      */
     static get DATABASE_URL() {
-        const port = 8012; // Change this to your server port
-        return `http://localhost:${port}/data/restaurants.json`;
+        /* const port = 8012; // Change this to your server port
+         return `http://localhost:${port}/data/restaurants.json`;*/
+        return 'https: //mzlovelyj.github.io/frontend-nanodegree-restaurant-site/data/restaurants.json`
     }
 
     /**
